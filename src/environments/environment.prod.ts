979d11c3fbddc3b5bb06a6db253f6e0f9f13e625
Othmane FETTACHE <70212249+FienexSistorm@users.noreply.github.com>
2025-04-13 12:35:13 +0100

    [master] configure personal firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyCyO-it99iHGAAhhqgJwL1PXKxPgxtWKUY",
-    authDomain: "todo-51be8.firebaseapp.com",
-    databaseURL: "https://todo-51be8.firebaseio.com",
-    projectId: "todo-51be8",
-    storageBucket: "todo-51be8.appspot.com",
-    messagingSenderId: "364093659824",
-    appId: "1:364093659824:web:019b4442ea81e243b9b975",
+    apiKey: "AIzaSyAtYF2Y84XPAkTZwItzlyscQFXnSgTiKBA",
+    authDomain: "todo-app-7f9fd.firebaseapp.com",
+    projectId: "todo-app-7f9fd",
+    storageBucket: "todo-app-7f9fd.firebasestorage.app",
+    messagingSenderId: "772477827091",
+    appId: "1:772477827091:web:cbcb8f0317908d5c3da7e6"
     measurementId: "G-EQ11C9G3B8"
   }
 };
